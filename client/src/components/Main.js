6a9fa332029478c206dc0ace2efb438d792ea153
zunayed-arp <zunayed.arp@gmail.com>
2023-01-19 03:40:54 +0600

    setup root router components

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import {Link} from 'react-router-dom';
 
 export default function Main() {
+
+
+  const inputRef = useRef(null);
+
   return (
-    <div>Main</div>
+    <div className='container'>
+      <h1 className='title text-light'>Quiz Application</h1>
+
+      <ol>
+        <li>You be asked 10 quesitons one after another.</li>
+        <li>10 points is awarded for the correct answer.</li>
+        <li>Each question has three options. You can choose only one options.</li>
+        <li>You can review and change answers before the quiz finish.</li>
+        <li>The result will be declared at the end of the quiz.</li>
+      </ol>
+
+
+    <form id="form">
+      <input ref={inputRef} type="text" placeholder='Username'/>
+    </form>
+
+    <div className='start'>
+      <Link className='btn' to={'quiz'}>Start Quiz</Link>
+    </div>
+
+    </div>
+
   )
 }
