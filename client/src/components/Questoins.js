e4bbaf348324b79cf5f30011e7955580c7d5d6f1
zunayed-arp <zunayed.arp@gmail.com>
2023-01-20 11:54:17 +0600

    questtions componenet added with data

--- client/src/components/Questoins.js
+++ client/src/components/Questoins.js
@@ -1,39 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import data from '../database/data';
+import React, { useEffect, useState } from "react";
+import data from "../database/data";
 
 export default function Questoins() {
+  const [checked, setChecked] = useState(undefined);
 
-    const [checked,setChecked]=useState(undefined);
+  const question = data[0];
 
-    const question = data[0]
+  useEffect(() => {
+    console.log(question);
+  });
 
-    useEffect(()=>{
-        console.log(question)
-    });
-
-
-    const onSelect = () =>{
-        // setChecked(true)
-        console.log('radio button change')
-    };
-
+  const onSelect = () => {
+    // setChecked(true)
+    console.log("radio button change");
+  };
 
   return (
-    <div className='questions'>
-        <h2 className="text-light"> Simple Question 1</h2>
-        <ul key={question.id}>
-            <li>
-                <input
-                type="radio"
-                value={false}
-                name="options"
-                id="q1-option"
-                onChange={onSelect()}
-                />
-                <label htmlFor="q1-option" className="text-primary">options</label>
-                <div className="check checked"></div>
-            </li>
-        </ul>
+    <div className="questions">
+      <h2 className="text-light">{question.question}</h2>
+      <ul key={question.id}>
+        {question.options.map((q, i) => (
+          <li key={i}>
+            <input
+              type="radio"
+              value={false}
+              name="options"
+              id={`q${i}-option`}
+              onChange={onSelect()}
+            />
+            <label htmlFor={`q${i}-option`} className="text-primary">
+              {q}
+            </label>
+            <div className="check"></div>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
