d49c2e06c467dbbc2f0f9609ae5eae04c9fb0ed9
zunayed-arp <zunayed.arp@gmail.com>
2023-01-20 12:46:08 +0600

    result components added

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import '../styles/Result.css';
 
 export default function Result() {
   return (
-    <div>Result</div>
+    <div className='container'>
+      <h1 className="title text-light">Quiz Application</h1>
+
+      <div className="result flex-center">
+        <div className="flex">
+          <span>Username</span>
+          <span className="bold">Zunayd Dev</span>
+        </div>
+        <div className="flex">
+          <span>Username</span>
+          <span className="bold">Zunayd Dev</span>
+        </div>
+        <div className="flex">
+          <span>Username</span>
+          <span className="bold">Zunayd Dev</span>
+        </div>
+      </div>
+    </div>
   )
 }
